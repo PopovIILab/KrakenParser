de461ff925cf26970dc6cddaeeff6c6281f7f359
Ilia Popov <iljapopov17@gmail.com>
2026-06-04 09:31:54 +0200

    refactor(init): add explicit type annotation to __all__ export sequence

--- krakenparser/__init__.py
+++ krakenparser/__init__.py
@@ -2,7 +2,7 @@ from .kpplot.clustermap import clustermap
 from .kpplot.stackedbar import stacked_barplot
 from .kpplot.streamgraph import streamgraph
 
-__all__ = [
+__all__: list[str] = [
     "stacked_barplot",
     "streamgraph",
     "clustermap",
