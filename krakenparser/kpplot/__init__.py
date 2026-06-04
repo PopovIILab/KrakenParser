f190b3e826ae706de7d6ef160bc6050daed4a0c3
Ilia Popov <iljapopov17@gmail.com>
2026-06-04 09:38:02 +0200

    refactor(kpplot): expand sub-package initialization namespace and enforce explicit export typing

--- krakenparser/kpplot/__init__.py
+++ krakenparser/kpplot/__init__.py
@@ -1,3 +1,14 @@
 from .base import KpPlotBase
+from .clustermap import KpClustermap, clustermap
+from .stackedbar import KpStackedBarplot, stacked_barplot
+from .streamgraph import KpStreamgraph, streamgraph
 
-__all__ = ["KpPlotBase"]
+__all__: list[str] = [
+    "KpPlotBase",
+    "KpClustermap",
+    "clustermap",
+    "KpStackedBarplot",
+    "stacked_barplot",
+    "KpStreamgraph",
+    "streamgraph",
+]
